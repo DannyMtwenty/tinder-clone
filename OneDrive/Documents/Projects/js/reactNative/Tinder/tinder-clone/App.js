8c317dc382c564da5e2f017dc0a1d74075cb31c3
DannyMtwenty <dannywanyoike@gmail.com>
2022-07-11 00:54:29 +0300

    custom tinder card components e.g users

--- OneDrive/Documents/Projects/js/reactNative/Tinder/tinder-clone/App.js
+++ OneDrive/Documents/Projects/js/reactNative/Tinder/tinder-clone/App.js
@@ -1,26 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,ImageBackground, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, ImageBackground, Text, View } from "react-native";
+import Card from "./src/components/TinderCard";
+import users from "./assets/data/users"
 
+const jeff = {
+  name: "jesff",
+  bio: "I am the wajakoye the fith",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+};
 export default function App() {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-      <ImageBackground
-        source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
-        }}
-        style={styles.image}>
-
-<View style={styles.cardInner}>
-       
-          <Text style={styles.name}>Wajakoya</Text>
-          <Text style={styles.bio}>The wakojoya fifth do you think?</Text>
-          </View>
-       
-      </ImageBackground>
-
-     
-      </View>
+      <Card user={users[2]} />
     </View>
   );
 }
@@ -28,48 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image :{
-    width: '100%',
-    height: '100%',
-    borderRadius: 15,
-    
-    overflow: 'hidden',
-
-    justifyContent: 'flex-end',
-    
-  },
-  card :{
-    width: '95%',
-    height: '70%',
-    borderRadius: 15,
-    backgroundColor: '#fefefe',
-
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-
-    elevation: 11,
-
-  },
-  name :{
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold', 
-  },
-  bio :{
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,   
-  },
-  cardInner: {
-    padding: 10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
