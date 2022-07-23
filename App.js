67b5bccb171d180efd41d27e6d4e2f7f1c9c266f
DannyMtwenty <dannywanyoike@gmail.com>
2022-07-23 14:39:18 +0300

    working anim and gesture

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ImageBackground, Text, View } from "react-native";
+import { StyleSheet, ImageBackground, Text, View, Pressable } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
@@ -9,22 +9,28 @@ import Animated, {
   interpolate,
   withSpring,
   runOnJS,
-} from 'react-native-reanimated';
+} from "react-native-reanimated";
 import Card from "./src/components/TinderCard";
 import users from "./assets/data/users";
 
 
 
-
-const sharedValue = useSharedValue(1);
-const cardStyle = useAnimatedStyle(() => ({
-  opacity: sharedValue.value,
-}));
-
 export default function App() {
+
+  const sharedValue = useSharedValue(1);
+  const cardStyle = useAnimatedStyle(() => ({
+    opacity: sharedValue.value,
+  }));
+
   return (
     <View style={styles.pageContainer}>
-      <Card user={users[2]} />
+      <Animated.View style={[styles.animatedCard,cardStyle]}>
+        <Card user={users[2]} />
+      </Animated.View>
+    
+    <Pressable onPress={() => sharedValue.value = Math.random()} style={{marginBottom : 60}}>
+       <Text>Change me</Text>
+      </Pressable>
     </View>
   );
 }
@@ -35,5 +41,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  animatedCard: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+    justifyContent : 'center',
+    alignItems : 'center',
+   
   },
 });
